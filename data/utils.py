fixed noised masking + normalizing factors for node/edge features

data/utils.py
@@ -58,20 +58,20 @@ def prepare_json_dataset(lobsterlist, filepath):
     f.close()         
 
 # turn Lobster into Data 
-def encode(lobster, max_nodes):
-    x = torch.empty(max_nodes, 4)
+def encode(lobster, max_nodes, max_nbrs, max_md1, max_md2, num_node_feat=4, color_norm_factor=2, edge_attr_norm_factor=2):
+    x = torch.empty(max_nodes, num_node_feat)
     for i in range(lobster.card):
-            x[i][0] = lobster.colors[i]
-            x[i][1] = len(lobster.adj[i]) # degree
-            x[i][2] = lobster.md1[i] # distance of furthest node
-            x[i][3] = lobster.md2[i] # distance of 2nd furthest node
+            x[i][0] = lobster.colors[i] / color_norm_factor
+            x[i][1] = len(lobster.adj[i]) / (max_nbrs + 1)
+            x[i][2] = lobster.md1[i] / (max_md1 + 1)
+            x[i][3] = lobster.md2[i] / (max_md2 + 1)
     edge_index = []
     edge_attr = []
     for i in range(lobster.card):
         for j in range(lobster.card):
             if i != j: # no loops
                 edge_index.append([i, j])
-                attr = 1 if j in lobster.adj[i] else 0
+                attr = 1 / edge_attr_norm_factor if j in lobster.adj[i] else 0
                 edge_attr.append(attr)
     for i in range(max_nodes):
         for j in range(max_nodes):

diffusion/ddim_sample.py
@@ -13,7 +13,7 @@ def get_edge_index(max_nodes, n=1):
                 continue
             edge_index.append([i, j])    
     edge_index = torch.tensor(edge_index)
-    return edge_index
+    return edge_index.t().contiguous()
 
 def sample(config, model, noise_scheduler, num_inference_steps=1000, n=1):
 
@@ -22,8 +22,8 @@ def sample(config, model, noise_scheduler, num_inference_steps=1000, n=1):
     noise_scheduler.set_timesteps(num_inference_steps)
 
     x_shape = (config.max_n_nodes, config.in_node_nf)
-    e_shape = (config.max_n_nodes * (config.max_n_nodes - 1),
-                       config.in_edge_nf)
+    e_shape = (config.max_n_nodes * config.max_n_nodes,
+                config.in_edge_nf)
 
     input_x = torch.randn(x_shape, device=config.device)
     input_e = torch.randn(e_shape, device=config.device)
@@ -33,23 +33,22 @@ def sample(config, model, noise_scheduler, num_inference_steps=1000, n=1):
     edge_mask[:n * (n - 1)] = 1 # loopless, directed graph
     
     edge_index = get_edge_index(max_nodes=config.max_n_nodes, n=n)
+    edge_index = edge_index.to(device=config.device)
 
     for t in noise_scheduler.timesteps:
         input_x = input_x * node_mask
         input_e = input_e * edge_mask
 
-        print(input_x.shape, input_e.shape, edge_index.shape)
         with torch.no_grad():
             node_noise_res, node_edge_res = model(x=input_x, 
                                                 edge_attr=input_e,
-                                                t=torch.tensor(t),
+                                                t=t.to(device=config.device),
                                                 edge_index=edge_index,
                                                 batch_size=1,
                                                 node_mask=node_mask,
                                                 edge_mask=edge_mask
                                                 )
 
-        print('gyatt')
         input_x = noise_scheduler.step(node_noise_res, t, input_x, eta=config.eta).prev_sample
         input_e = noise_scheduler.step(node_edge_res, t, input_e, eta=config.eta).prev_sample
 

diffusion/ddpm.py
@@ -45,6 +45,11 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
                 0, noise_scheduler.config.num_train_timesteps, (bs,), device=accelerator.device, 
                 dtype=torch.int64
             )
+            
+            timesteps = torch.randint(
+                0, 1, (bs,), device=accelerator.device, 
+                dtype=torch.int64
+            )
 
             # Add noise to the clean images according to the noise magnitude at each timestep
             # (this is the forward diffusion process)
@@ -55,9 +60,10 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
                                                     edge_noise.reshape(bs, num_edges, -1), 
                                                     timesteps).reshape(batch.edge_index.size(1), -1)
             
+            node_noise = node_noise * node_mask
+            edge_noise = edge_noise * edge_mask
             noisy_nodes = noisy_nodes * node_mask
             noisy_edges = noisy_edges * edge_mask
-
             with accelerator.accumulate(model):
                 # Predict the noise residual
                 noise_node_pred, noise_edge_pred = model(x=noisy_nodes, 
@@ -68,8 +74,8 @@ def train_loop(config, model, noise_scheduler, optimizer, train_dataloader, lr_s
                                                         node_mask=node_mask,
                                                         edge_mask=edge_mask
                                                         )
-                loss = F.mse_loss(noise_node_pred, noisy_nodes) + \
-                        F.mse_loss(noise_edge_pred, noisy_edges)
+                loss = F.mse_loss(noise_node_pred, node_noise) + \
+                        F.mse_loss(noise_edge_pred, edge_noise)
                 
                 if type(loss) is torch.nan:
                     print('node:', noise_node_pred)

inference.py
@@ -10,8 +10,8 @@ from diffusion.ddim_sample import sample
 class InferenceConfig:
     max_n_nodes = None
     data_filepath = 'data/dataset/lobsters.json'
-    scheduler_filepath = "diffusion/models/scheduler/scheduler_config.json"  # the model name locally and on the HF Hub
-    checkpoint_filepath = 'diffusion/models/gnn/checkpoint_epoch_50.pth'
+    scheduler_filepath = "diffusion/models/scheduler_config.json"  # the model name locally and on the HF Hub
+    checkpoint_filepath = 'diffusion/models/gnn/checkpoint_epoch_10.pth'
     eta = 1 # DDPM
 
     device = 'mps'
@@ -48,14 +48,18 @@ model = LobsterDynamics(in_node_nf=config.in_node_nf,
 model = torch.load(config.checkpoint_filepath)
 model.eval()
 
-n = 10
+n = 30
 s = 1000 # num inference steps
 
-sample(config=config, 
-       model=model, 
-       noise_scheduler=noise_scheduler, 
-       num_inference_steps=s,
-       n=n)
+print(noise_scheduler)
+nodes, edges = sample(config=config, 
+                        model=model, 
+                        noise_scheduler=noise_scheduler, 
+                        num_inference_steps=s,
+                        n=n)
+
+print(nodes[:n+1])
+print(edges[:n * n])
 
 
 

train_ddpm.py
@@ -66,7 +66,8 @@ model = LobsterDynamics(in_node_nf=config.in_node_nf,
                         aggregation_method=config.aggregation_method)
 
 noise_scheduler = DDPMScheduler(num_train_timesteps=1000, 
-                                beta_schedule='squaredcos_cap_v2')
+                                beta_schedule='squaredcos_cap_v2'
+                                )
 
 optimizer = torch.optim.AdamW(model.parameters(), lr=config.learning_rate)
 lr_scheduler = get_cosine_schedule_with_warmup(